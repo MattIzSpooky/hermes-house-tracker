7fb19f6cfcd997f9de261845fb5e22faf2927db7
MattIzSpooky <matthijsk2000@pm.me>
2026-06-12 14:43:07 +0200

    feat(hermes-backend): add active/deleted listing queries to ListingRepository

--- hermes-backend/src/main/java/com/kropholler/dev/hermes/listing/internal/ListingRepository.java
+++ hermes-backend/src/main/java/com/kropholler/dev/hermes/listing/internal/ListingRepository.java
@@ -1,5 +1,7 @@
 package com.kropholler.dev.hermes.listing.internal;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -7,4 +9,6 @@ import java.util.UUID;
 
 public interface ListingRepository extends JpaRepository<Listing, UUID> {
     Optional<Listing> findByFundaId(String fundaId);
+    Page<Listing> findAllByDeletedAtIsNull(Pageable pageable);
+    void deleteAllByDeletedAtIsNotNull();
 }
